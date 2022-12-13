c5c5c88b6100398b54a9d28aaea4a7b3ba3c2df3
Luan Lima <luan_alp@hotmail.com>
2022-12-13 09:03:30 -0300

    CheckPoint 3 - Prog Imperativa VF

--- Atividades-2-bimestre/Prog-imperativa/checkPoint3/index.js
+++ Atividades-2-bimestre/Prog-imperativa/checkPoint3/index.js
@@ -5,40 +5,31 @@ class Aluno {
     this.nome = nome;
     this.faltas = faltas;
     this.notas = notas;
-  }
-}
-  /*2-Na função construtora crie o método calcularMedia que retorna a média de suas notas. 
+  };
+
+   /*2-Na função construtora crie o método calcularMedia que retorna a média de suas notas. 
 Também terá um método chamado faltas, que simplesmente aumenta o número de faltas em 1. 
 Crie alguns alunos para testar a sua função construtora.*/
-const escola = {
-  alunos: [],
-  buscarAluno(alunoBusca) {
-    for (let aluno of this.alunos) {
-      if (alunoBusca === aluno.nome) {
-        return aluno;
-      }
-    }
-  },
-  calcularMedia(alunoMedia) {
-    const alunoBusca = this.buscarAluno(alunoMedia);
-    if (alunoBusca) {
+
+  calcularMedia() {
       let media = 0;
       let soma = 0;
-      for (let i = 0; i < alunoBusca.notas.length; i++) {
-        soma += alunoBusca.notas[i];
+      for (let i = 0; i < this.notas.length; i++) {
+        soma += this.notas[i];
       }
-      media = soma / alunoBusca.notas.length;
-      console.log("A média das notas do aluno",alunoBusca.nome,"é:",media.toFixed(2));
-    }
-  },
-  faltas(alunoFalta) {
-    const alunoBusca = this.buscarAluno(alunoFalta);
-    if (alunoBusca) {
-      alunoBusca.faltas++;
-      console.log("O total de faltas do aluno",alunoBusca.nome,"agora é:",alunoBusca.faltas);
-    }
-  },
-};
+      media = soma / this.notas.length;
+      return (`${media.toFixed(2)}`);;
+  };
+  addfaltas() {
+      this.faltas++;
+    };
+    
+  };
+
+/*const aluno1 = new Aluno("José", 6, [5.0, 7.5, 8.0, 9.0]);
+console.log(aluno1.calcularMedia());
+aluno1.addfaltas();
+console.log(aluno1.faltas);*/
 
   /*3-crie o objeto literal curso que tem como atributos: nome do curso 
 (string), nota de aprovação (number), faltas máximas (number) e uma 
@@ -48,7 +39,7 @@ const curso = {
   nomeCurso: "Curso CTD",
   notaAprovacao: 7.0,
   faltasMaximas: 5,
-  estudantes: escola.alunos,
+  alunos: [],
 
     /*4-Crie o método que permite adicionar alunos à lista do curso, ou seja, quando chamamos nosso método 
   em nosso objeto curso, deverá adicionar um aluno a mais na propriedade lista de estudantes do objeto 
@@ -56,7 +47,15 @@ const curso = {
 
   adicionarAluno(nome, faltas, notas = []) {
     const novoAluno = new Aluno(nome, faltas, notas);
-    this.estudantes.push(novoAluno);
+    this.alunos.push(novoAluno);
+  },
+
+  buscarAluno(alunoBusca) {
+    for (let aluno of this.alunos) {
+      if (alunoBusca === aluno.nome) {
+        return aluno;
+      }
+    }
   },
 
   /*5-Crie um método para o objeto curso que receba um aluno (como parâmetro) e retorne true se ele aprovou no curso ou false em caso de reprovação. 
@@ -64,18 +63,10 @@ Para ser aprovado, o aluno tem que ter uma média igual ou acima da nota de apro
 Se tiver a mesma quantidade, tem que estar 10% acima da nota de aprovação.*/
 
   avaliador(alunoAvaliado) {
-    for (let estudante of this.estudantes) {
-      if (alunoAvaliado === estudante.nome) {
-        const alunoBusca = estudante;
+    
+        const alunoBusca = this.buscarAluno(alunoAvaliado);
+        const media = alunoBusca.calcularMedia();
         
-        media = 0;
-        soma = 0;
-
-        for (let i = 0; i < alunoBusca.notas.length; i++) {
-          soma += alunoBusca.notas[i];
-        }
-        media = soma / alunoBusca.notas.length;
-
         if (
           media >= this.notaAprovacao && alunoBusca.faltas < this.faltasMaximas) {
           console.log("O aluno", alunoBusca.nome,"foi aprovado.","Retorno do programa: ",true);
@@ -88,47 +79,27 @@ Se tiver a mesma quantidade, tem que estar 10% acima da nota de aprovação.*/
           console.log("O aluno ",alunoBusca.nome,"foi reprovado.","Retorno do programa: ",false);
           return false;
         }
-      }
-    }
-  },
+      },
 
   /*6- Crie um método para o objeto curso que percorra a lista de estudantes e retorne um array de booleanos com os resultados
   se os alunos aprovaram ou não.*/
 
-  listaAvaliador(){
+  listaAvaliador() {
     let aprovados = [];
-    let reprovados = [];
-
-    for(let estudante of this.estudantes){
-      const alunoBusca = estudante;
-      let media = 0;
-      let soma = 0;
-
-      for (let i = 0; i < alunoBusca.notas.length; i++) {
-        soma += alunoBusca.notas[i];
-      }
-      media = soma / alunoBusca.notas.length;
-
-      if (
-        media >= this.notaAprovacao && alunoBusca.faltas < this.faltasMaximas) {
-        aprovados.push("Aluno "+alunoBusca.nome+":",true);
-      } else if (
-        media >= this.notaAprovacao + (this.notaAprovacao * 0.1) && alunoBusca.faltas == this.faltasMaximas) {
-        aprovados.push("Aluno "+alunoBusca.nome+":",false);
-      } else {
-        reprovados.push("Aluno "+alunoBusca.nome+":",false);
-      }
-    }  
-    console.log("Lista de aprovados: ",aprovados);
-    console.log("Lista de reprovados: ",reprovados);
+    for(let aluno of this.alunos){
+      aprovados.push({ nome: aluno.nome, aprovado: this.avaliador(aluno.nome) })
+    }
   }
 };
+
 curso.adicionarAluno("Caue", 4, [10, 9, 7]);
 curso.adicionarAluno("Luan", 3, [5, 6, 9]);
-curso.adicionarAluno("Zé Ricardo", 6, [10, 10, 10]);
-curso.adicionarAluno("Eduardo", 7, [8, 9.5, 6]);
+curso.adicionarAluno("Ederson", 6, [10, 10, 10]);
+curso.adicionarAluno("Eduardo", 2, [8, 9.5, 6]);
+curso.adicionarAluno("Vanessa", 2, [8, 9.5, 10]);
+curso.adicionarAluno("Pedro", 5, [7, 9, 10]);
 
-//escola.calcularMedia("Luan");
-//escola.faltas("Zé Ricardo");
 //curso.avaliador("Eduardo");
-curso.listaAvaliador();
+//curso.avaliador("Caue");
+console.log(curso.listaAvaliador());
+//console.log(curso.alunos);
