b96cf995f6054c8b924d22603a74fd6ca2860497
Luan Lima <luan_alp@hotmail.com>
2023-02-22 23:02:43 -0300

    Aulas matéria Front-II

---
+++ Atividades-3-bimestre/Front-end II/Aula 1/script.js
@@ -0,0 +1,13 @@
+//declarando uma variável
+var Username = 'Ivin Rodrigues'
+let age = 22
+const job = 'Tech'
+const welcomeMessage = 'Olá' + Username;
+/*
+console.log(Username);
+window.prompt("Olá, qual o seu nome?");
+*/
+
+var Username = window.prompt('Olá, qual é o seu nome');
+console.log('Olá' + Username);
+alert(welcomeMessage);
